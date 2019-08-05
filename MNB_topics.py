fb1309a28c140c920aa44abe7f450432993f5ddb
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2019-08-06 04:24:33 +1000

    Excel to dump data

--- MNB_topics.py
+++ MNB_topics.py
@@ -1,3 +1,6 @@
+from sklearn.utils.multiclass import unique_labels
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
 import time
 import pandas as pd
 import csv
@@ -54,6 +57,63 @@ def myTokenizer(sample):
     return new_words
 
 
+def plot_confusion_matrix(y_true, y_pred, classes,
+                          normalize=False,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix'
+        else:
+            title = 'Confusion matrix, without normalization'
+
+    # Compute confusion matrix
+    cm = confusion_matrix(y_true, y_pred)
+    # Only use the labels that appear in the data
+    classes = classes[unique_labels(
+        y_true.astype('int'), y_pred.astype('int'))]
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    fig, ax = plt.subplots()
+    im = ax.imshow(cm, interpolation='nearest', cmap=cmap)
+    ax.figure.colorbar(im, ax=ax)
+    # We want to show all ticks...
+    ax.set(xticks=np.arange(cm.shape[1]),
+           yticks=np.arange(cm.shape[0]),
+           # ... and label them with the respective list entries
+           xticklabels=classes, yticklabels=classes,
+           title=title,
+           ylabel='True label',
+           xlabel='Predicted label')
+
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
+
+    # Loop over data dimensions and create text annotations.
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i in range(cm.shape[0]):
+        for j in range(cm.shape[1]):
+            ax.text(j, i, format(cm[i, j], fmt),
+                    ha="center", va="center",
+                    color="white" if cm[i, j] > thresh else "black")
+    fig.tight_layout()
+    return ax
+
+
+class_names = np.asarray(list(range(0, 20)))
+
 try:
     size = int(sys.argv[1])
 except IndexError:
@@ -69,7 +129,7 @@ X = bag_of_words.toarray()
 # creating target classes
 Y = np.array([])
 for text in df.id:
-    Y = np.append(Y, text)
+    Y = np.append(Y, int(text)-10000)
 # First 1500 for training set, last 500 for test set
 X_train, X_test, y_train, y_test = train_test_split(
     X, Y, test_size=0.25, shuffle=False)
@@ -87,9 +147,11 @@ training_time = (time.time() - start_time)
 # print(f1_score(y_test, y_pred, average='macro'))
 
 y_pred = model.predict(X_test)
-# print(classification_report(y_test, y_pred))
+print(classification_report(y_test, y_pred))
 # print('Accuracy score:', accuracy_score(y_test, y_pred))
 testtime = time.time() - start_time
+plot_confusion_matrix(y_test, y_pred, classes=class_names,
+                      title='Confusion matrix, without normalization')
 test_report = classification_report(y_test, y_pred, output_dict=True)
 
 start_time = time.time()
@@ -115,3 +177,15 @@ test_str_output += f"{testtime:.4f}"
 train_str_output += f"{trainingtime:.4f}"
 print(test_str_output.rstrip())
 print(train_str_output.rstrip())
+
+
+np.set_printoptions(precision=2)
+
+# Plot non-normalized confusion matrix
+
+
+# Plot normalized confusion matrix
+# plot_confusion_matrix(y_train, y_pred, classes=class_names, normalize=True,
+#                       title='Normalized confusion matrix')
+
+plt.show()
