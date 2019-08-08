85f40da0dc6896f9aca45b4fd4098ee1f55b7fa9
Kanadech Jirapongtanavech <kanadech.king@gmail.com>
2019-08-08 14:27:04 +1000

    Before replacing table 1 in docx

--- DT_sentiment.py
+++ DT_sentiment.py
@@ -70,12 +70,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 count = CountVectorizer(preprocessor=myPreprocessor,
                         lowercase=False, tokenizer=myTokenizer, max_features=size)
-# bag_of_words = count.fit_transform(text_data)
-# print(count.get_feature_names())
-# size = len(count.vocabulary_)
-# print(len(count.vocabulary_))
-# X = bag_of_words.toarray()
-# creating target classes
+
 X_train = count.fit_transform(X_train).toarray()
 print("----------Train vector------------", len(X_train))
 print(X_train)
@@ -98,14 +93,14 @@ training_time = (time.time() - start_time)
 # print(f1_score(y_test, y_pred, average='macro'))
 
 y_pred = model.predict(X_test)
-print(classification_report(y_test, y_pred))
+# print(classification_report(y_test, y_pred))
 # print('Accuracy score:', accuracy_score(y_test, y_pred))
 testtime = time.time() - start_time
 test_report = classification_report(y_test, y_pred, output_dict=True)
 
 start_time = time.time()
 y_pred = model.predict(X_train)
-print(classification_report(y_train, y_pred))
+# print(classification_report(y_train, y_pred))
 # print('Accuracy score:', accuracy_score(y_train, y_pred))
 trainingtime = (time.time() - start_time + training_time)
 
